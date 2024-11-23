d109bf27051acaae801119b2c05bcc7b18acccb8
Sammuell3 <samuelsongsytlbry@gmail.com>
2024-11-23 11:34:20 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,50 @@
 import Image from "next/image";
-
+import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa';
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const links = [
+    { name: "GitHub", url: "https://github.com/Sammuell3", icon: <FaGithub className="inline-block mr-2" /> },
+    { name: "Instagram", url: "https://instagram.com/sa_uell3/profilecard/", icon: <FaInstagram className="inline-block mr-2" /> },
+    { name: "LinkedIn", url: "https://linkedin.com/in/samuel-barros-219378296", icon: <FaLinkedin className="inline-block mr-2"/>},
+  ];
+  
+
+  return (
+    <div className="flex items-center flex-col mx-auto w-full justify-center mt-16">
+      {/*Cabeçalho com foto de perfil e descrição*/}
+        <Image
+          className="w-24 h-24 rounded-full mx-auto"
+          src="/foto.png"
+          alt="Foto de perfil"
+          width={128}
+          height={128}
+        />
+        <h1 className="flex justify-center items-center text-2x1 font-semibold w-full text-gray my-5">Samuel Barros</h1>
+
+        {/*Lista de link*/}
+        <div className="mt-8 w-full max-w-md">
+
+          <ul>
+            {links.map((link, index) => (
+              <li key={index} className="mb-4">
+                <a
+                  href={link.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="block bg-gray-50 text-black p-4 w-full rounded-full text-center hover:scale-105 transition-all border border-gray-700"
+                >
+                  {link.name} {link.icon}
+                </a>
+              </li>
+            ))}
+          </ul>
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <footer>
+        <p className="text-sm font-light">
+          Feito com <span className="text-yellow-300">Next.js e Tailwind CSS</span> por Samuel Barros 🧐
+        </p>
+        </footer>
     </div>
   );
 }
